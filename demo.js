57ae59859dadd127a011e771ab89010f7e775a76
Jackson Tian <shyvo1987@gmail.com>
2017-02-06 17:42:21 +0800

    Add demo.js

---
+++ demo.js
@@ -0,0 +1,16 @@
+// npm install aliyun-api-gateway --save
+
+const co = require('co');
+const SimpleClient = require('aliyun-api-gateway').SimpleClient;
+
+const appcode = '你自己的AppCode';
+const client = new SimpleClient(appcode);
+
+const url = 'http://ali-bankcard.showapi.com/bankcard';
+
+co(function* () {
+  var result = yield client.get(url, {
+    data: {kahao: '6215982582010042122'}
+  });
+  console.log(result);
+});
